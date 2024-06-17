54d04db056cce5c0761b9340280f5b26a6337654
Ahmed Choulak <ahmedchoulak2004@gmail.com>
2024-06-17 23:34:13 +0100

    la logique de js
    il manque maintenant le message de win qui va s'afficher lorsque un des joueurs aura gagné

--- script.js
+++ script.js
@@ -1,1 +1,99 @@
-const cases = documer
+const cases = Array.from(document.querySelectorAll(".case"));
+
+class Player {
+    constructor(name, signe) {
+        this.name = name;
+        this.signe = signe;
+    }
+}
+
+class Game {
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.game = [null, null, null, null, null, null, null, null, null];
+        this.gameCases = cases;
+        this.playerTurn = true;
+    }
+
+    resetGame() {
+        cases.forEach(element => {
+            element.textContent = "";
+        });
+        this.game = [null, null, null, null, null, null, null, null, null];
+        
+    }
+
+    playTurn() {
+        if (this.playerTurn) {
+            this.playerTurn = false;
+            return this.player1;
+        } else {
+            this.playerTurn = true;
+            return this.player2;
+        }
+    }
+
+    setCase(index) {
+        if (this.game[index] === null) {
+            const currentPlayer = this.playTurn();
+            this.gameCases[index].textContent = currentPlayer.signe;
+            this.game[index] = currentPlayer.signe;
+            return true;
+        }
+        return false;
+    }
+
+    checkWin() {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let condition of winConditions) {
+            const [a, b, c] = condition;
+            if (this.game[a] && this.game[a] === this.game[b] && this.game[a] === this.game[c]) {
+                return this.game[a] === this.player1.signe ? this.player1 : this.player2;
+            }
+        }
+
+        return null;
+    }
+    checkDraw(){
+        if(!this.game.includes(null)){
+            return true;
+        }
+        return false;
+    }
+}
+
+const player1 = new Player("ahmed", 'X');
+const player2 = new Player("walid", 'O');
+const game = new Game(player1, player2);
+
+cases.forEach((cell, index) => {
+    cell.addEventListener('click', () => {
+        const caseSet = game.setCase(index);
+        if (caseSet) {
+            const winner = game.checkWin();
+            const draw = game.checkDraw();
+            if (winner) {
+                setTimeout(() => {
+                    game.resetGame();
+                }, 3000);
+            }
+            if(draw){
+                setTimeout(() => {
+                    game.resetGame();
+                }, 3000);
+            }
+
+        }
+    });
+});
